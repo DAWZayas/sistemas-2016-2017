40a9fdd2e0a74ed2b0116a60a094a972337599fd
Javier Pérez <surtich@gmail.com>
2017-02-01 12:57:11 +0100

    Añadido apartado a ejercicio3.sh

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -4,3 +4,10 @@ cat /etc/passwd | cut -f1 -d: | xargs -I{} bash -c "echo -n '{} '; echo {} | gre
 echo "Mostrar el nombre del usuario con más vocales versión que saca varios usuarios si tienen el máximo"
 cat /etc/passwd | cut -f1 -d: | xargs -I{} bash -c "echo -n '{} '; echo {} | grep -o [aeiouAEIOU] | wc -l" | grep "^.* $(cat /etc/passwd | cut -f1 -d: | xargs -I{} bash -c "echo {} | grep -o [aeiouAEIOU] | wc -l" | sort -n | tail -1)$" | cut -f1 -d' '
 
+echo "Mostrar el nombre del usuario con más vocales diferentes (versión que saca varios usuarios si tienen el máximo)"
+cat /etc/passwd | cut -f1 -d: | xargs -I{} bash -c "echo -n '{} ';echo '{}'| grep -Eo [aeiou] | sort | uniq |wc -l " | grep -E "^.* $(cat /etc/passwd | cut -f1 -d: | xargs -I{} bash -c "echo '{}'| grep -Eo [aeiou] | sort | uniq |wc -l " | sort -n | tail -1)$"| cut -f1 -d" "
+
+echo "Mostrar el nombre de los usuarios que comienzan y terminan por la misma letra"
+cat /etc/passwd | cut -f1 -d: | grep -E "^(.).*\1$"
+
+
